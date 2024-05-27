b35ed301d33e3036d2046617b4b20919680e7bcf
Namman Shukla <namman.shukla@my.jcu.edu.au>
2024-05-27 23:28:34 +0530

    initialized repo with template

---
+++ src/mynav.js
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "./assets/web-logo.svg";
+import navIcon1 from "./assets/github-mark-white.svg";
+import navIcon3 from "./assets/linkedin.svg";
+import { BrowserRouter as Router } from "react-router-dom";
+
+export const MyNavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
+  return (
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+            <Navbar.Brand href="#home">
+                <img src={logo} className="logo"/>
+            </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link
+                href="#about"
+                className={
+                  activeLink === "about" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("about")}>
+                About Me
+              </Nav.Link>
+
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}>
+                Skills
+              </Nav.Link>
+
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}>
+                Projects
+              </Nav.Link>
+
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="https://github.com/NammanShukla">
+                  <img src={navIcon1} alt="GitHub" />
+                </a>
+                <a href="https://www.linkedin.com/in/namman-shukla-7b0531275/">
+                  <img src={navIcon3} alt="Linkedin" />
+                </a>
+              </div>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  );
+};
