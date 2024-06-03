9ad5cff9f31a9885b2564d909a157a59cd1e16c7
NammanShukla <Nammanshukla262@gmail.com>
2024-06-03 22:00:05 +0530

    Remove Skill Section

--- src/mynav.js
+++ src/mynav.js
@@ -51,15 +51,6 @@ export const MyNavBar = () => {
               </Nav.Link>
 
               <Nav.Link
-                href="#skills"
-                className={
-                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("skills")}>
-                Skills
-              </Nav.Link>
-
-              <Nav.Link
                 href="#projects"
                 className={
                   activeLink === "projects"
