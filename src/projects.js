04af2939c27d5fc2423f275efedac4aaa326c141
NammanShukla <Nammanshukla262@gmail.com>
2024-06-03 22:01:02 +0530

    Refactor Content and Add Modal Button

--- src/projects.js
+++ src/projects.js
@@ -1,11 +1,13 @@
 import { Container } from "react-bootstrap";
 import Carousel from "react-multi-carousel"; 
+import modal from "./modal";
 import "react-multi-carousel/lib/styles.css"; 
 import { Row, Col } from "react-bootstrap"; 
 import id8ai from "./assets/id8-ai.svg"; 
 import id8website from "./assets/id8-website.svg";
 import alchemist from "./assets/alchemist-post.svg";
 import solasphere from "./assets/solasphere-post.svg";
+import { ArrowUpRightSquare } from "react-bootstrap-icons";
 
 export const Projects = () => {
     const responsive = {
@@ -37,27 +39,35 @@ export const Projects = () => {
                             Projects
                         </h2>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br>
+                            Here are all the projects that I've been involved in throughout my career !<br></br>
                         </p>
                         <Carousel responsive={responsive} infinite={true} className="project-slider">
                             <div>
                                 <img src={id8ai} alt="image" />
-                                <h5>iD8's A.I Bot</h5>
+                                <h4>iD8's A.I Bot</h4>
+                                <h5> Using a Locally hosted LLM with an integrated Speech to Text transcription process written in Python</h5>
+                                <button className="project-button"> <h6><ArrowUpRightSquare size={15}  className="arrow-more"  />  Show More </h6> </button>
                             </div>
 
                             <div>
                                 <img src={alchemist} alt="image" />
-                                <h5>Alchemist: Reciepe Generator</h5>
+                                <h4>Alchemist: Reciepe Generator</h4>
+                                <h5> Using Object Recognition (Python) and a Language Model, developed an application that will recognise ingredients in the fridge and give you a random recipe you can make. </h5>
+                                <button className="project-button"> <h6><ArrowUpRightSquare size={15}  className="arrow-more" /> Show More</h6> </button>
                             </div>
 
                             <div>
                                 <img src={solasphere} alt="image" />
-                                <h5>Solasphere: Relaxtion Pod</h5>
+                                <h4>Solasphere: Relaxtion Pod</h4>
+                                <h5> Case Study on Poor life balance offering a Japanese Style Multipurpose Isolation Pod to help aleviate the stress</h5>
+                                <button className="project-button"> <h6><ArrowUpRightSquare size={15}  className="arrow-more" /> Show More</h6> </button>
                             </div>
 
                             <div>
                                 <img src={id8website} alt="image" />
-                                <h5>iD8's Website</h5>
+                                <h4>iD8's Website</h4>
+                                <h5> Hosted a Website using AWS Lightsail and Routed the domain using Route 53 for the IT Club @JCUS </h5>
+                                <button className="project-button"> <h6> <ArrowUpRightSquare size={15} className="arrow-more" /> Show More</h6> </button>
                             </div>
                         </Carousel>
 
