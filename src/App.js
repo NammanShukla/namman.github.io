6ecbc0f5bc424d8e8d2e88b3650dd13c58cf72c3
NammanShukla <nammanshukla262@gmail.com>
2025-02-02 22:28:33 +0530

    Import views.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { MyNavBar } from "./mynav.js";
 import { Banner } from "./banner.js"; 
 import { Projects } from "./projects.js"
 import './App.css';
+import './views.css'
 
 function App() {
   return (
